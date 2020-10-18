e45016d86d4f81e6babcf53c4c3d82f7cb63fc43
Wajeehgillani <hassangillani1994@gmail.com>
2020-10-18 22:17:25 +0500

    added build

---
+++ build/precache-manifest.c63502db1f7b35a63f454736710c02c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb61858766a0057091a6cdcd46451f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965b6e95ef98b716be84",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84c4d5418b2efcad85d1",
+    "url": "/static/js/2.bc46e115.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.bc46e115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965b6e95ef98b716be84",
+    "url": "/static/js/main.ef65e0d3.chunk.js"
+  },
+  {
+    "revision": "73103f698f94fc4061c5",
+    "url": "/static/js/runtime-main.fabf54ab.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  }
+]);
