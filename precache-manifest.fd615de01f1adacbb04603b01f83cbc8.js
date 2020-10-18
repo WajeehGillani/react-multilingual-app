ae6ecf4774ad5e05f1d570db4aab537f32124bbe
Wajeehgillani <hassangillani1994@gmail.com>
2020-10-18 23:09:05 +0500

    Updates

---
+++ precache-manifest.fd615de01f1adacbb04603b01f83cbc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4255548a60258d9aa6f4dd76cc2beda8",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/index.html"
+  },
+  {
+    "revision": "965b6e95ef98b716be84",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84c4d5418b2efcad85d1",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/js/2.bc46e115.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/js/2.bc46e115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965b6e95ef98b716be84",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/js/main.ef65e0d3.chunk.js"
+  },
+  {
+    "revision": "bd237f7d7d633162e3a3",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/js/runtime-main.8611cbc9.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/WajeehGillani.github.io/react-multilingual-app/static/media/getFetch.013f0b90.cjs"
+  }
+]);
